08d3318dc4805bf2880ac764854961f82d7459b1
Robpol86 <robpol86@gmail.com>
2017-04-17 12:10:09 -0700

    Just the node class.

---
+++ btree.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""Binary tree algorithms."""
+
+
+class Node(object):
+    """A tree node."""
+
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def main():
+    """Main function."""
+    pass
+
+
+if __name__ == '__main__':
+    main()
